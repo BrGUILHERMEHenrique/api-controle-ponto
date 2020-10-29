a2699fae3dc757b1560bf10afe733ae9176f9811
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2020-10-29 08:49:04 -0300

    Criando: lógica total apontamento

--- src/main/java/com/dois/pack/api/services/ApontamentoService.java
+++ src/main/java/com/dois/pack/api/services/ApontamentoService.java
@@ -2,10 +2,10 @@ package com.dois.pack.api.services;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Period;
 import java.util.List;
-import static java.time.temporal.ChronoUnit.MINUTES;
-import static java.time.temporal.ChronoUnit.HOURS;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -24,75 +24,209 @@ public class ApontamentoService {
 
 	@Autowired
 	ApontamentoRepository apontamentoRepository;
-	
+
 	@Autowired
 	FuncionarioHorarioRepository funcionarioHorarioRepository;
-	
-	@Autowired 
+
+	@Autowired
 	HorarioDetalhesRepository horarioDetalhesReposotory;
-	
+
 	@Transactional
-	public List<Apontamento> getAll (){
+	public List<Apontamento> getAll() {
 		return apontamentoRepository.findAll();
 	}
-	
+
 	@Transactional
-	public Apontamento getById (Integer id) {
+	public Apontamento getById(Integer id) {
 		Apontamento apontamento = apontamentoRepository.findById(id).get();
 		return apontamento;
 	}
-	
+
 	@Transactional
-	public Apontamento create (Apontamento apontamento) {
-		System.out.println(apontamento.getData());
-		System.out.println("idFuncionario apontamento ," + apontamento.getIdFuncionario().getId());
-		HorarioDetalhes horarioDetalhes;
-		Integer idHorario = funcionarioHorarioRepository.getIdHorario(
-				apontamento.getIdFuncionario().getId(), 
+	public Apontamento create(Apontamento apontamento) {
+
+		Integer idHorario = funcionarioHorarioRepository.getIdHorario(apontamento.getIdFuncionario().getId(),
 				apontamento.getData());
-		System.out.println("idHorario ," + idHorario);
-		
-		if(idHorario != null) {
+		Integer idFuncionario = apontamento.getIdFuncionario().getId();
+
+		if (idHorario != null) {
 			Integer qtdHorarios = horarioDetalhesReposotory.getCount(idHorario);
-			FuncionarioHorario funcionarioHorario = funcionarioHorarioRepository.getFuncionario(
-					apontamento.getIdFuncionario().getId(), idHorario);
-			System.out.println("FuncionarioHorario: " + funcionarioHorario.getVigenciaInicial());
-			System.out.println("Quanti Horarios: " + qtdHorarios);
+			FuncionarioHorario funcionarioHorario = funcionarioHorarioRepository.getFuncionario(idFuncionario,
+					idHorario);
+
 			LocalDate vigenciaInicial = LocalDate.of(funcionarioHorario.getVigenciaInicial().getYear(),
-														funcionarioHorario.getVigenciaInicial().getMonth(),
-														funcionarioHorario.getVigenciaInicial().getDayOfMonth());
-			
+					funcionarioHorario.getVigenciaInicial().getMonth(),
+					funcionarioHorario.getVigenciaInicial().getDayOfMonth());
+
 			Period period = Period.between(vigenciaInicial, apontamento.getData());
-			System.out.println("Periodo: " + period.getDays());
-			
-			Integer result = (period.getDays() % qtdHorarios) + funcionarioHorario.getCodigoInicial() - 1;
-			
-			System.out.println("conta: " + result);
-			if(result == 0) {
-				result = qtdHorarios;
-			}
-			if(result > qtdHorarios) {
-				result -= qtdHorarios;
-			}
-			
-			HorarioDetalhes idHorarioDetalhe = horarioDetalhesReposotory.getWithCodigoDia(result);
-			
-			apontamento.setIdHorarioDetalhes(idHorarioDetalhe);
-			
-//			Long hora1 = HOURS.between(apontamento.getEntrada1(), apontamento.getSaida1());
-//			
-//			Long hora2 = HOURS.between(apontamento.getEntrada2(), apontamento.getSaida2());
-//			
-//			Long hora1HD = HOURS.between(idHorarioDetalhe.getEntrada1(), idHorarioDetalhe.getSaida1());
-//			
-//			Long hora2HD = HOURS.between(idHorarioDetalhe.getEntrada2(), idHorarioDetalhe.getSaida2());
-//			
-//			System.out.println((hora1 + hora2) - (hora1HD + hora2HD));
-			
+
+			Integer result = calculateResult(period.getDays(), funcionarioHorario.getCodigoInicial(), qtdHorarios);
+
+			HorarioDetalhes horarioDetalhe = horarioDetalhesReposotory.getWithCodigoDia(result);
+
+			apontamento.setIdHorarioDetalhes(horarioDetalhe);
+
+			calculateHours(apontamento, horarioDetalhe);
+
 		}
-		
-		return apontamento;
+
+		return apontamentoRepository.save(apontamento);
 	}
-	
-	
+
+	private Integer calculateResult(Integer days, Integer codigoInicial, Integer qtdHorarios) {
+
+		Integer result = ((days + 1 % qtdHorarios) + codigoInicial) - 1;
+
+		if (result == 0) {
+			result = qtdHorarios;
+		}
+		if (result > qtdHorarios) {
+			result -= qtdHorarios;
+		}
+		return result;
+	}
+
+	private Integer calculateAccount(Apontamento apontamento, HorarioDetalhes horarioDetalhe) {
+		int durationHours = getHour(apontamento.getEntrada1(), apontamento.getSaida1());
+		int durationMinutes = getMinute(apontamento.getEntrada1(), apontamento.getSaida1());
+		int hoursToMinutes = hoursToMinutes(durationHours, durationMinutes);
+		System.out.println("Horas * 60: " + (hoursToMinutes + durationMinutes));
+
+		int durationHour2 = getHour(apontamento.getEntrada2(), apontamento.getSaida2());
+		int durationMinutes2 = getMinute(apontamento.getEntrada2(), apontamento.getSaida2());
+		int hoursToMinutes2 = hoursToMinutes(durationHour2, durationMinutes2);
+		System.out.println("Horas * 60 2 : " + (hoursToMinutes2 + durationMinutes2));
+
+		int durationHourHD = getHour(horarioDetalhe.getEntrada1(), horarioDetalhe.getSaida1());
+		int durationMinutesHD = getMinute(horarioDetalhe.getEntrada1(), horarioDetalhe.getSaida1());
+		int hoursToMinutesHD = hoursToMinutes(durationHourHD, durationMinutesHD);
+		System.out.println("Horas * 60 3 : " + (hoursToMinutesHD + durationMinutesHD));
+
+		int durationHourHD2 = getHour(horarioDetalhe.getEntrada2(), horarioDetalhe.getSaida2());
+		int durationMinutesHD2 = getMinute(horarioDetalhe.getEntrada2(), horarioDetalhe.getSaida2());
+		int hoursToMinutesHD2 = hoursToMinutes(durationHourHD2, durationMinutesHD2);
+		System.out.println("Horas * 60 4: " + (hoursToMinutesHD2 + durationMinutesHD2));
+
+		int horas = sumDurations(hoursToMinutes, hoursToMinutes2);
+		int horasHD = sumDurations(hoursToMinutesHD, hoursToMinutesHD2);
+
+		int hora = horas / 60;
+		int minuto = horas % 60;
+		apontamento.setTotalTrabalhado(LocalTime.of(hora, minuto));
+
+		return (horas - horasHD);
+	}
+
+	private void calculateHours(Apontamento apontamento, HorarioDetalhes horarioDetalhe) {
+
+		if (verifyFolga(horarioDetalhe, apontamento))
+			return;
+
+		int contaHoras = calculateAccount(apontamento, horarioDetalhe);
+
+		verifyHours(apontamento, contaHoras);
+
+	}
+
+	private int getMinute(LocalTime entrada, LocalTime saida) {
+		return saida.getMinute() - entrada.getMinute();
+	}
+
+	private int getHour(LocalTime entrada, LocalTime saida) {
+		return saida.getHour() - entrada.getHour();
+	}
+
+	private int sumDurations(int duration1, int duration2) {
+		return duration1 + duration2;
+	}
+
+	private int hoursToMinutes(int hours, int minutes) {
+		return (hours * 60) + minutes;
+	}
+
+	private void verifyHours(Apontamento apontamento, int horas) {
+		System.out.println("Horas: " + horas);
+
+		LocalTime horaApurada;
+
+		if (horas < 0) {
+			horas = Math.abs(horas);
+			int hora = horas / 60;
+			int minuto = horas % 60;
+			System.out.println("Horas atraso: " + hora);
+			System.out.println("minuto atraso: " + minuto);
+			horaApurada = LocalTime.of(hora, minuto);
+			System.out.println(horaApurada);
+			apontamento.setSaldoAtraso(horaApurada);
+			apontamento.setSaldoHe(LocalTime.of(0, 0));
+
+		} else {
+			int hora = horas / 60;
+			int minuto = horas % 60;
+			System.out.println("Horas HE: " + hora);
+			System.out.println("minuto HE: " + minuto);
+			horaApurada = LocalTime.of(hora, minuto);
+			apontamento.setSaldoHe(horaApurada);
+			apontamento.setSaldoAtraso(LocalTime.of(0, 0));
+
+		}
+
+	}
+
+	public boolean verifyFolga(HorarioDetalhes horarioDetalhe, Apontamento apontamento) {
+		int durationHours = getHour(apontamento.getEntrada1(), apontamento.getSaida1());
+		int durationMinutes = getMinute(apontamento.getEntrada1(), apontamento.getSaida1());
+		int hoursToMinutes = hoursToMinutes(durationHours, durationMinutes);
+
+		int durationHours2 = getHour(apontamento.getEntrada2(), apontamento.getSaida2());
+		int durationMinutes2 = getMinute(apontamento.getEntrada2(), apontamento.getSaida2());
+		int hoursToMinutes2 = hoursToMinutes(durationHours2, durationMinutes2);
+
+		int horas = sumDurations(hoursToMinutes, hoursToMinutes2);
+		if (horarioDetalhe.getFolga()) {
+			int hora = horas / 60;
+			int minuto = horas % 60;
+			LocalTime horaTrabalhadaFolga = LocalTime.of(hora, minuto);
+			apontamento.setSaldoHe(horaTrabalhadaFolga);
+			apontamento.setTotalTrabalhado(horaTrabalhadaFolga);
+			apontamento.setSaldoAtraso(LocalTime.of(0, 0));
+			return true;
+		}
+		return false;
+	}
+
+	@Transactional
+	public Apontamento update(Apontamento apontamento, Integer id) {
+		Apontamento apontamentoAtualizado = apontamentoRepository.findById(id).get();
+		if (apontamentoAtualizado != null) {
+			if (apontamento.getEntrada1() != null) {
+				apontamentoAtualizado.setEntrada1(apontamento.getEntrada1());
+			}
+			if (apontamento.getEntrada2() != null) {
+				apontamentoAtualizado.setEntrada2(apontamento.getEntrada2());
+			}
+			if (apontamento.getSaida1() != null) {
+				apontamentoAtualizado.setSaida1(apontamento.getSaida1());
+			}
+			if (apontamento.getSaida2() != null) {
+				apontamentoAtualizado.setSaida2(apontamento.getSaida2());
+			}
+			Integer contaHoras = calculateAccount(apontamentoAtualizado, apontamentoAtualizado.getIdHorarioDetalhes());
+			System.out.println("Conta horas put: " + contaHoras);
+			verifyHours(apontamentoAtualizado, contaHoras);
+
+		}
+		return apontamentoRepository.save(apontamentoAtualizado);
+	}
+
+	@Transactional
+	public boolean delete(Integer id) {
+		Optional<Apontamento> apontamento = apontamentoRepository.findById(id);
+		if (apontamento.isPresent()) {
+			apontamentoRepository.delete(apontamento.get());
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
